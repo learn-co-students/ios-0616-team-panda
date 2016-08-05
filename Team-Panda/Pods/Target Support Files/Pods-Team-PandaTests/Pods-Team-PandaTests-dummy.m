4ec9848d742b1d6753ec6609db6abd2e98048b44
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-08-04 20:56:10 -0400

    Initial Commit

---
+++ Team-Panda/Pods/Target Support Files/Pods-Team-PandaTests/Pods-Team-PandaTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Team_PandaTests : NSObject
+@end
+@implementation PodsDummy_Pods_Team_PandaTests
+@end
---
+++ Team-Panda/Pods/Target Support Files/Pods-Team-PandaUITests/Pods-Team-PandaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Team_PandaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Team_PandaUITests
+@end
