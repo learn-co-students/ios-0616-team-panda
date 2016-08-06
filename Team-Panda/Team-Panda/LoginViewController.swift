b919f709d91eb3adb6daf6b0e8c9f06268a3f7be
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-08-05 23:09:13 -0400

    Setting up the basics for login view #1

---
+++ Team-Panda/Team-Panda/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  Team-Panda
+//
+//  Created by Lloyd W. Sykes on 8/5/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func submitButtonTapped(sender: AnyObject) {
+    }
+    
+}
