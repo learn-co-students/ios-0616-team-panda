d3536e986c0cb9f6d9be09c94c90290db5eb1143
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-08-06 19:13:50 -0400

    Created views added constraints setup button action #1

--- Team-Panda/Team-Panda/LoginViewController.swift
+++ Team-Panda/Team-Panda/LoginViewController.swift
@@ -10,15 +10,65 @@ import UIKit
 import SnapKit
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.createAndAddViews()
+        self.textFieldConstraints()
     }
     
-    @IBAction func submitButtonTapped(sender: AnyObject) {
+    @IBAction func submitTapped(sender: UIButton!) {
+        print("Submit Tapped!")
     }
     
+    func createAndAddViews() {
+        
+        self.usernameTextField = UITextField()
+        self.usernameTextField.placeholder = "Username"
+        self.passwordTextField = UITextField()
+        self.passwordTextField.placeholder = "Password"
+        
+        let button = UIButton()
+        button.setTitle("Submit", forState: .Normal)
+        button.setTitleColor(.blueColor(), forState: .Normal)
+        button.addTarget(self, action: #selector(self.submitTapped), forControlEvents: .TouchUpInside)
+        self.view.addSubview(button)
+        button.snp_makeConstraints { (make) in
+            make.centerY.equalTo(self.view).offset(20)
+            make.centerX.equalTo(self.view)
+            make.width.equalTo(75)
+            make.height.equalTo(25)
+        }
+        
+        self.view.addSubview(self.usernameTextField)
+        self.view.addSubview(self.passwordTextField)
+        
+        // button.backgroundColor = UIColor.brownColor()
+        // self.usernameTextField.backgroundColor = UIColor.cyanColor()
+        // self.passwordTextField.backgroundColor = UIColor.greenColor()
+        
+    }
+    
+    func textFieldConstraints() {
+        
+        self.usernameTextField.textAlignment = NSTextAlignment.Center
+        self.passwordTextField.textAlignment = NSTextAlignment.Center
+        
+        self.usernameTextField.snp_makeConstraints { (make) in
+            make.centerY.equalTo(self.view).offset(-50)
+            make.centerX.equalTo(self.view)
+            make.width.equalTo(300)
+            make.height.equalTo(20)
+        }
+        
+        self.passwordTextField.snp_makeConstraints { (make) in
+            make.centerY.equalTo(self.view).offset(-15)
+            make.centerX.equalTo(self.view)
+            make.width.equalTo(300)
+            make.height.equalTo(20)
+        }
+    }
 }
