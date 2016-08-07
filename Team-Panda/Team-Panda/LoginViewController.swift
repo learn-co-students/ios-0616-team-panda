3ebbe44b94713745aa1f61a1f4447564514cb6ee
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-08-07 11:45:57 -0400

    Facebook login working #1

--- Team-Panda/Team-Panda/LoginViewController.swift
+++ Team-Panda/Team-Panda/LoginViewController.swift
@@ -18,9 +18,8 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.createAndAddViews()
-        self.textFieldConstraints()
     }
-    
+
     @IBAction func submitTapped(sender: UIButton!) {
         print("Submit Tapped!")
     }
@@ -29,29 +28,32 @@ class LoginViewController: UIViewController {
         
         self.usernameTextField = UITextField()
         self.passwordTextField = UITextField()
+//        self.usernameTextField.becomeFirstResponder()
+//        self.passwordTextField.resignFirstResponder()
         self.usernameTextField.placeholder = "Username"
         self.passwordTextField.placeholder = "Password"
+        let submitButton = UIButton()
+        submitButton.setTitle("Submit", forState: .Normal)
+        submitButton.setTitleColor(.blueColor(), forState: .Normal)
+        submitButton.addTarget(self, action: #selector(self.submitTapped), forControlEvents: .TouchUpInside)
+        self.facebookLoginButton()
         
-        let button = UIButton()
-        button.setTitle("Submit", forState: .Normal)
-        button.setTitleColor(.blueColor(), forState: .Normal)
-        button.addTarget(self, action: #selector(self.submitTapped), forControlEvents: .TouchUpInside)
-        self.view.addSubview(button)
-        button.snp_makeConstraints { (make) in
+        self.view.addSubview(self.usernameTextField)
+        self.view.addSubview(self.passwordTextField)
+        self.view.addSubview(submitButton)
+        
+        self.textFieldConstraints()
+        submitButton.snp_makeConstraints { (make) in
             make.centerY.equalTo(self.view).offset(20)
             make.centerX.equalTo(self.view)
             make.width.equalTo(75)
             make.height.equalTo(25)
         }
         
-        self.view.addSubview(self.usernameTextField)
-        self.view.addSubview(self.passwordTextField)
-        
         self.view.backgroundColor = UIColor.cyanColor()
         self.usernameTextField.backgroundColor = UIColor.whiteColor()
         self.passwordTextField.backgroundColor = UIColor.whiteColor()
-        button.backgroundColor = UIColor.brownColor()
-        
+        submitButton.backgroundColor = UIColor.brownColor()
     }
     
     func textFieldConstraints() {
@@ -71,6 +73,34 @@ class LoginViewController: UIViewController {
             make.centerX.equalTo(self.view)
             make.width.equalTo(300)
             make.height.equalTo(20)
+        }
+    }
+    
+    func facebookLoginButton() {
+        let facebookLoginButton = UIButton()
+        facebookLoginButton.setTitle("Login with Facebook!", forState: .Normal)
+        facebookLoginButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        facebookLoginButton.addTarget(self, action: #selector(self.fbLoginButtonTapped), forControlEvents: .TouchUpInside)
+        facebookLoginButton.backgroundColor = UIColor.redColor()
+        self.view.addSubview(facebookLoginButton)
+        facebookLoginButton.snp_makeConstraints { (make) in
+            make.centerY.equalTo(self.view).offset(50)
+            make.centerX.equalTo(self.view)
+            make.width.equalTo(200)
+            make.height.equalTo(25)
+        }
+    }
+    
+    func fbLoginButtonTapped() {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logInWithReadPermissions(["public_profile"], fromViewController: self) { (result, error) in
+            if error != nil {
+                print ("There was a processing error \(error.localizedDescription)")
+            } else if result.isCancelled {
+                print("Cancelled")
+            } else {
+                print("Log in successful!")
+            }
         }
     }
 }
