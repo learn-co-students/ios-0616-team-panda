3959ca6262eca0a1e75907ee50f4d44c24c25b20
sheafk <sheafureyking@gmail.com>
2016-08-09 11:43:27 -0400

    Moved SettingsTableViewController into a SettingsVC. Set up switch statement for each selection.

--- Team-Panda/Team-Panda/SettingsViewController.swift
+++ Team-Panda/Team-Panda/SettingsViewController.swift
@@ -8,41 +8,57 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    class SettingsTableView: UITableView {
+    var tableView: UITableView  =   UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.frame = CGRectMake(0, 50, 320, 200);
+        tableView.delegate      =   self
+        tableView.dataSource    =   self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "settingsCell")
+        self.view.addSubview(self.tableView)
+        self.view.backgroundColor = UIColor.blueColor()
+        tableView.center = self.view.center
+        tableView.layer.cornerRadius = 10
         
-        var settingsOptions = ["Update Profile Info", "Log Out", "Switch User", "Refresh Questionaire"]
+    }
+    
+    var settingsOptions = ["Update Profile Info", "Log Out", "Switch User", "Refresh Questionaire"]
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingsOptions.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        override func viewDidLoad() {
-            super.viewDidLoad()
+        let cell = tableView.dequeueReusableCellWithIdentifier("settingsCell", forIndexPath: indexPath) as UITableViewCell!
+        
+        cell.textLabel?.text = settingsOptions[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        switch indexPath.row {
+        case 0:
+            print("Update Profile Info")
+        case 1:
+            print("Log Out")
+        case 2:
+            print("Switch User")
+        case 3:
+            print("Refresh Questionaire")
+        default:
+            print("Opps. Not Available")
         }
-        
-        // MARK: - Table view data source
-        override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-            return 1
-        }
-        
-        override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return settingsOptions.count
-        }
-        
-        
-        override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            
-            let cell = tableView.dequeueReusableCellWithIdentifier("settingsCell", forIndexPath: indexPath) as UITableViewCell!
-            
-            cell.textLabel?.text = settingsOptions[indexPath.row]
-            
-            return cell
-        }
-        
-        override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-            
-            //implementation here
-        }
-        
     }
     
 }
