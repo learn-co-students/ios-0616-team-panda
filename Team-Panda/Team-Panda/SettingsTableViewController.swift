bda5e26c0688e9b52d16dc8a358172a4f715fa95
sheafk <sheafureyking@gmail.com>
2016-08-09 09:59:08 -0400

    Simplified Settings

--- Team-Panda/Team-Panda/SettingsTableViewController.swift
+++ Team-Panda/Team-Panda/SettingsTableViewController.swift
@@ -9,46 +9,27 @@
 
 import UIKit
 
-class SettingsTableViewController: UITableViewController {
+//Put this tableView inside of a VC
+
+
+
+class SettingsTableView: UITableView {
     
-    var settingsOptions = ["Reset", "Log Out", "Switch User", "Refresh Questionaire"]
+    var settingsOptions = ["Update Profile Info", "Log Out", "Switch User", "Refresh Questionaire"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.sectionHeaderHeight = 60
     }
     
     // MARK: - Table view data source
-    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 2
+        return 1
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        switch section {
-        case 0:
-            return 1
-        case 1:
-            return 3
-        default:
-            assert(false, "section \(section)")
-            return 0
-        }
-}
-    
-    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        
-        switch section {
-        case 0:
-            return "Career Settings"
-        case 1:
-            return "User Settings"
-        default:
-            assert(false, "section \(section)")
-            return "Did not work"
-        }
+        return settingsOptions.count
     }
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
--- Team-Panda/Team-Panda/SettingsViewController.swift
+++ Team-Panda/Team-Panda/SettingsViewController.swift
@@ -2,7 +2,7 @@
 //  SettingsViewController.swift
 //  Team-Panda
 //
-//  Created by Lloyd W. Sykes on 8/5/16.
+//  Created by Flatiron School on 8/9/16.
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
@@ -10,4 +10,39 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    
+    class SettingsTableView: UITableView {
+        
+        var settingsOptions = ["Update Profile Info", "Log Out", "Switch User", "Refresh Questionaire"]
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+        }
+        
+        // MARK: - Table view data source
+        override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+            return 1
+        }
+        
+        override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return settingsOptions.count
+        }
+        
+        
+        override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCellWithIdentifier("settingsCell", forIndexPath: indexPath) as UITableViewCell!
+            
+            cell.textLabel?.text = settingsOptions[indexPath.row]
+            
+            return cell
+        }
+        
+        override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            
+            //implementation here
+        }
+        
+    }
+    
 }
