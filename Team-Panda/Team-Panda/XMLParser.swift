aed57cdec2dadc566d58891ae10135b495f50dd1
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-08-16 10:12:38 -0400

    Implemented initial XML parsing functions #4

---
+++ Team-Panda/Team-Panda/XMLParser.swift
@@ -0,0 +1,33 @@
+//
+//  XMLParser.swift
+//  Team-Panda
+//
+//  Created by Lloyd W. Sykes on 8/15/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+
+class XMLParser: NSObject, NSXMLParserDelegate {
+
+    
+    // let parser: NSXMLParser!
+
+    
+    
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        
+    }
+    
+    func parser(parser: NSXMLParser, foundCharacters string: String) {
+        
+    }
+    
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        
+    }
+    
+    
+    
+}
