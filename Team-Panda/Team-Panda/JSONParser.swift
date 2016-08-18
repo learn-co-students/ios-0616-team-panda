960ffc1aa8ee420c85ddab44b2fe926a13a409ec
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-08-18 11:17:59 -0400

    Beginning JSON parsing updated Constants file

---
+++ Team-Panda/Team-Panda/JSONParser.swift
@@ -0,0 +1,26 @@
+//
+//  JSONParser.swift
+//  Team-Panda
+//
+//  Created by Lloyd W. Sykes on 8/18/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class JSONParser {
+    
+    
+//    func parsingJSON() {
+//        
+//        if let path = NSBundle.mainBundle().pathForResource("Occupation-Dictionary", ofType: "json") {
+//            do {
+//                let data = try NSData(contentsOfURL: NSURL(fileURLWithPath: path), options: NSDataReadingOptions.DataReadingMappedIfSafe)
+//            }
+//        }
+//        
+//    }
+
+    
+}
