28328f85ff972c754a79b1aaf92529ef7fc4df23
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-08-30 21:37:31 -0400

    Using NSURLSession instead of Alamofire

--- Team-Panda/Team-Panda/BLSAPIClient.swift
+++ Team-Panda/Team-Panda/BLSAPIClient.swift
@@ -14,36 +14,144 @@ class BLSAPIClient {
     
     class func getMultipleOccupationsWithCompletion(params: [String: AnyObject], completion: (NSDictionary?, NSError?) -> ()) {
         
-        Alamofire.request(.POST, Secrets.apiURL, parameters: params, encoding: .JSON, headers: headers).responseJSON { (blsResponse) in
+        guard let url = NSURL(string: Secrets.apiURL) else {
+            fatalError("There was an issue unwrapping the NSURL")
+        }
+        
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        var err: NSError?
+        
+        do {
             
-            if let error = blsResponse.result.error {
-                print("There was an error pulling multiple occupations from the API: \(error.localizedDescription)")
-                completion(nil, error)
-            }
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(params, options: []) as NSData
             
-            if let json = blsResponse.result.value {
-                guard let careerResults = json as? NSDictionary else {
-                    return
-                }
-                completion(careerResults, nil)
+        } catch {
+            
+            if let err = err as NSError? {
+                print("There's been an error adding the parameters to the request: \(err.localizedDescription)")
             }
         }
+        
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            if let data = data {
+                do {
+                    
+                    let json = try NSJSONSerialization.JSONObjectWithData(data, options: []) as? NSDictionary
+                    
+                    if let json = json {
+                        
+                        NSOperationQueue.mainQueue().addOperationWithBlock({
+                            completion(json, nil)
+                        })
+                    }
+                    
+                } catch {
+                    print("There was a problem getting the JSON in dataTaskWithRequest")
+                }
+            } else {
+                if let error = error {
+                    
+                    print("There's been an error trying to get the JSON: \(error.localizedDescription)")
+                    completion(nil, error)
+                }
+            }
+        }
+        
+        task.resume()
     }
     
     class func getLocationQuotientforJobWithCompletion(params: [String : AnyObject], completion: (NSDictionary?, NSError?)->()) {
         
-        Alamofire.request(.POST, Secrets.apiURL, parameters: params, encoding: .JSON, headers: headers).responseJSON { (blsLQresponse) in
-            if let error = blsLQresponse.result.error {
-                print("There was an error pulling the Location Quotient from the API: \(error.localizedDescription)")
-                completion(nil, error)
-            }
+        guard let url = NSURL(string: Secrets.apiURL) else {
+            fatalError("There was an issue unwrapping the NSURL")
+        }
+        
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        var err: NSError?
+        
+        do {
             
-            if let json = blsLQresponse.result.value {
-                guard let lqResults = json as? NSDictionary else {
-                    return
-                }
-                completion(lqResults, nil)
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(params, options: []) as NSData
+            
+        } catch {
+            
+            if let err = err as NSError? {
+                print("There's been an error adding the parameters to the request: \(err.localizedDescription)")
             }
         }
+        
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            if let data = data {
+                do {
+                    
+                    let json = try NSJSONSerialization.JSONObjectWithData(data, options: []) as? NSDictionary
+                    
+                    if let json = json {
+                        
+                        NSOperationQueue.mainQueue().addOperationWithBlock({
+                            completion(json, nil)
+                        })
+                    }
+                    
+                } catch {
+                    print("There was a problem getting the JSON in dataTaskWithRequest")
+                }
+                
+            } else {
+                
+                if let error = error {
+                    
+                    print("There's been an error trying to get the JSON: \(error.localizedDescription)")
+                    completion(nil, error)
+                }
+            }
+        }
+        
+        task.resume()
     }
+    
+    //        class func getMultipleOccupationsWithCompletion(params: [String: AnyObject], completion: (NSDictionary?, NSError?) -> ()) {
+    //
+    //            Alamofire.request(.POST, Secrets.apiURL, parameters: params, encoding: .JSON, headers: headers).responseJSON { (blsResponse) in
+    //
+    //                if let error = blsResponse.result.error {
+    //                    print("There was an error pulling multiple occupations from the API: \(error.localizedDescription)")
+    //                    completion(nil, error)
+    //                }
+    //
+    //                if let json = blsResponse.result.value {
+    //                    if let careerResults = json as? NSDictionary {
+    //                        completion(careerResults, nil)
+    //                    }
+    //
+    //                }
+    //            }
+    //        }
+    //
+    //        class func getLocationQuotientforJobWithCompletion(params: [String : AnyObject], completion: (NSDictionary?, NSError?)->()) {
+    //
+    //            Alamofire.request(.POST, Secrets.apiURL, parameters: params, encoding: .JSON, headers: headers).responseJSON { (blsLQresponse) in
+    //                if let error = blsLQresponse.result.error {
+    //                    print("There was an error pulling the Location Quotient from the API: \(error.localizedDescription)")
+    //                    completion(nil, error)
+    //                }
+    //
+    //                if let json = blsLQresponse.result.value {
+    //                    if let lqResults = json as? NSDictionary  {
+    //
+    //                        completion(lqResults, nil)
+    //                    }
+    //                }
+    //            }
+    //        }
 }
