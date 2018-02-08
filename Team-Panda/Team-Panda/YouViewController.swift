793d88b3c46da12787ea8152aa0284761b9f115f
Lloyd W. Sykes <lloyd1nspace@icloud.com>
2018-02-07 23:02:55 -0500

    More access control tweaks

--- Team-Panda/Team-Panda/YouViewController.swift
+++ Team-Panda/Team-Panda/YouViewController.swift
@@ -12,18 +12,18 @@ import SwiftSpinner
 
 final class YouViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var youTableView = UITableView()
-    let reuseIdentifier = "youCell"
-    let store = DataStore.store
-    var params : [String : Any] = [:]
-    lazy var statusBarView : UIView = UIView()
+    private let youTableView = UITableView()
+    private let reuseIdentifier = "youCell"
+    private let store = DataStore.store
+    var params = [String: Any]()
+    lazy private var statusBarView: UIView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.settingUpTableViewAndNavBar()
+        settingUpTableViewAndNavBar()
         SwiftSpinner.show("Loading your personalized results")
-        self.getSavedJobChoices {
+        getSavedJobChoices {
             SwiftSpinner.hide()
         }
     }
@@ -33,16 +33,15 @@ final class YouViewController: UIViewController, UITableViewDelegate, UITableVie
         tabBarController?.tabBar.tintColor = .flatMint
     }
     
-    func settingUpTableViewAndNavBar() {
+    private func settingUpTableViewAndNavBar() {
+        youTableView.delegate = self
+        youTableView.dataSource = self
+        youTableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        youTableView.backgroundColor = .flatMint
+        youTableView.accessibilityLabel = "tableView"
+        youTableView.accessibilityIdentifier = "tableView"
         
-        self.youTableView.delegate = self
-        self.youTableView.dataSource = self
-        self.youTableView.register(UITableViewCell.self, forCellReuseIdentifier: self.reuseIdentifier)
-        self.youTableView.backgroundColor = .flatMint
-        self.youTableView.accessibilityLabel = "tableView"
-        self.youTableView.accessibilityIdentifier = "tableView"
-        
-        self.view.addSubview(self.youTableView)
+        view.addSubview(youTableView)
         
         youTableView.translatesAutoresizingMaskIntoConstraints = false
         youTableView.safeLeadingAnchor.constraint(equalTo: view.safeLeadingAnchor).isActive = true
@@ -65,50 +64,40 @@ final class YouViewController: UIViewController, UITableViewDelegate, UITableVie
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = self.youTableView.dequeueReusableCell(withIdentifier: self.reuseIdentifier, for: indexPath)
+        let cell = youTableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
         let job = store.jobsResultsArray[indexPath.row]
         
         cell.textLabel?.text = job.occupation
         cell.textLabel?.font = UIFont.pandaFontLight(withSize: 16)
-        cell.backgroundColor = FlatMint()
+        cell.backgroundColor = .flatMint
         cell.textLabel?.adjustsFontSizeToFitWidth = false
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         SwiftSpinner.show("Loading Details", animated: true)
-        
         let jobDetail = JobDetailViewController()
-        
         jobDetail.job = store.jobsResultsArray[indexPath.row]
-        
-        self.navigationController?.show(jobDetail, sender: "")
-        
-        self.youTableView.deselectRow(at: indexPath, animated: true)
+        navigationController?.show(jobDetail, sender: "")
+        youTableView.deselectRow(at: indexPath, animated: true)
     }
     
-    fileprivate func getSavedJobChoices(_ completion : @escaping () -> ()) {
-        
+    private func getSavedJobChoices(_ completion: @escaping () -> ()) {
         if let currentPanda = store.tpUser {
-            self.params = DataSeries.createSeriesIDsFromSOC(currentPanda.socCodes, withDataType: DataSeries.annualMeanWage)
+            params = DataSeries.createSeriesIDsFromSOC(currentPanda.socCodes, withDataType: DataSeries.annualMeanWage)
         } else {
             print("Current user is nil when loading You from auto login.")
         }
         
-        self.store.getMultipleOccupationsWithCompletion(self.params) { job, error in
-            
+        store.getMultipleOccupationsWithCompletion(params) { [weak self] job, error in
             if let error = error {
-                
                 let alert = Constants.displayAlertWith("Network Error", message: error.localizedDescription, actionLabel: "Try Again", style: .cancel, actionHandler: {})
-                self.present(alert, animated: true, completion: nil)
+                self?.present(alert, animated: true, completion: nil)
                 completion()
-                
             } else {
-                
                 print("Finished API call. Updating table view.")
-                self.youTableView.reloadData()
+                self?.youTableView.reloadData()
                 completion()
             }
         }
